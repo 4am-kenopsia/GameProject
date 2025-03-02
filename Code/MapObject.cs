c950be38d6991fb00855eec8d017714757d18bb1
4am-kenopsia <tuukka.pelkonen@outlook.com>
2025-03-03 01:18:18 +0200

    AUUGHUAAAAHAAAAAA AAAAA jonkinlaine popup nappi

---
+++ Code/MapObject.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+namespace MapGame
+{
+    public abstract partial class MapObject : Node2D
+    {
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+        }
+
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(double delta)
+        {
+        }
+        
+        public bool SetPos(Vector2I mapPos)
+        {
+            if (MapArea.IsValidMapPos(mapPos))
+            {
+                Position = mapPos;
+                return true;
+            }
+            return false;
+        }
+        public abstract void OnClick();
+    }
+}
--- GameScenes/TestScene.cs
+++ GameScenes/TestScene.cs
@@ -5,15 +5,38 @@ namespace MapGame
 {
     public partial class TestScene : Node
     {
+        [Export] private string _popupMarkerPath = "res://Elements/Popup Marker/MarkerContainer.tscn";
+        private PackedScene _popupMarkerScene = null;
+        private PopupMarker _popupMarker = null;
+        
         // Called when the node enters the scene tree for the first time.
         public override void _Ready()
         {
-            GD.Print(ResourceManager.Instance._currentHappiness);
+            GD.Print(ResourceManager.Instance._currentMainResource);
+            AddMarker();
         }
 
         // Called every frame. 'delta' is the elapsed time since the previous frame.
         public override void _Process(double delta)
         {
+        }
+        public void AddMarker()
+        {
+            if (_popupMarker != null)
+            {
+                _popupMarker.QueueFree();
+                _popupMarker = null;
+            }
+            if (_popupMarkerScene == null)
+            {
+                _popupMarkerScene = ResourceLoader.Load<PackedScene>(_popupMarkerPath);
+            }
+            _popupMarker = _popupMarkerScene.Instantiate<PopupMarker>();
+            AddChild(_popupMarker);
+            Vector2I tempPos;
+            tempPos.X = 960;
+            tempPos.Y = 540;
+            _popupMarker.SetPos(tempPos);
         }
     }
 }
