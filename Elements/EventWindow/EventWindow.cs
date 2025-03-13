c2d0b4962455fc87e285608c7456eab22efee900
4am-kenopsia <tuukka.pelkonen@outlook.com>
2025-03-13 14:02:18 +0200

    Temp commit for event windows

--- Elements/EventWindow/EventWindow.cs
+++ Elements/EventWindow/EventWindow.cs
@@ -66,7 +66,7 @@ namespace MapGame
             _optionButtonText.Text = buttonContent;
             
             // Renames the newly created button
-            _optionButton.Name = "OptionButton" + optionNumber;            
+            _optionButton.Name = "OptionButton" + optionNumber;
         }
     }
 }
